05f752803036cfdc000fe1c9d1f3d1d581241098
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-02-18 16:54:08 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { JaydeepButton } from "../lib/main";
+import {JaydeepButton} from "../lib/main";
 import "./App.css";
 
 function App() {
-
-
   const connectWallet = async () => {
     try {
       const ethereum = (window as any).ethereum;
 
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" }); // request connection with accounts
+      await ethereum.request({
+        method: "eth_requestAccounts",
+      }); // request connection with accounts
       // console.log("Connected", accounts[0]);
       // setCurrentAccount(accounts[0]);
       // const chainId = await ethereum.request({ method: 'eth_chainId' });
-
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e);
     }
-  }
+  };
 
-  const address = "0xd7fdd1521fcf9571b4452295cf42e07a1479fcac552cae28e5a821ff69ce7ee1"
+  const address =
+    "0xd7fdd1521fcf9571b4452295cf42e07a1479fcac552cae28e5a821ff69ce7ee1";
 
   return (
     <>
@@ -29,7 +28,7 @@ function App() {
           text="helloworld"
           amount={100000000}
           address={address}
-          style={{ color: "red", fontSize: "40px" }}
+          style={{color: "red", fontSize: "40px"}}
         />
       </div>
     </>
